b8aa01dd42e9795ce9761c86a1a8048fa4c7ec04
Alexandre Magno AM. de Carvalho <alexandremagno@p4pro.com.br>
2020-10-12 13:11:44 -0300

    Criar model Negociacao

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,24 @@
+class Negociacao {
+    constructor(_data, _quantidade, _valor) {
+        //utilizando literal objects
+        Object.assign(this,{_quantidade, _valor});
+        this._data = new Date(_data.getTime());
+        Object.freeze(this);
+    }
+
+    get data(){
+        return  new Date(this._data.getTime());
+    }
+
+    get quantidadade(){
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
